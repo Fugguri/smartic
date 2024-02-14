055b2cbe83935cee94a08de815982fbb615d77ca
Fugguri <fygguri@gmail.com>
2024-02-14 17:22:24 +0300

    update

---
+++ utils/GoogleService.py
@@ -0,0 +1,27 @@
+import asyncio
+# import gspread_asyncio
+from google.oauth2.service_account import Credentials
+
+
+class GoogleService:
+    def __init__(self) -> None:
+        pass
+
+    async def save_lead(lead):
+        ...
+
+
+"""Инфа следующая
+
+По вопросам:
+
+1. goal
+2. work_with
+3. Education_important
+4. work_specific
+5. personal_improvements_goals
+6. budget
+7. start_education
+8. Имя
+9. Телефон
+10. Почта (почта должна быть обязательной для заполнения, без нее не получиться создать заявку в геткурс)"""
